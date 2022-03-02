136393ab198083fe21639ac5633ebb3ce82a3e47
Kyando2 <amehikoji@gmail.com>
2022-03-01 22:59:44 -0500

    add clock

---
+++ src/clock.jl
@@ -0,0 +1,25 @@
+# A Rudimentary clock built with papers and scissors
+mutable struct Clock
+    local_origin::DateTime # Time of clock creation
+    _last::Millisecond # last pass of _tick
+    delta::Millisecond # Cached version of Millisecond(now()-Clock.local_origin)
+    tick::Float64 # Frequency of the clock in hertz
+    _tick::Float64 # 1000/tick
+    on_fire::Function # Handle for when the clock fires
+end
+
+function Clock(f::Function; tick::Float64=100.0) # default to 100hz
+    if tick>1000.0 error("Tick must be below 1000hz") end
+    c = Clock(now(), Millisecond(0), Millisecond(0), tick, 1000.0/tick, f) 
+    @spawn begin 
+        clock_running = true
+        while clock_running 
+            c.delta = Millisecond(now()-c.local_origin)
+            if c.delta>Millisecond(c._tick)+c._last
+                res = f()
+                c._last = c.delta
+                if res == -1 clock_running = false end
+            end
+        end
+    end
+end
